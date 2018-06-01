a9da7f5216b6215e2496ee93855123da6ac8ad42
Rufus Russell <rrussell@Rufuss-MacBook-Pro.local>
2018-06-01 14:12:31 +0100

    configure webpack loaders, output, devSerger

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,8 +1,25 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var parentDir = path.join(__dirname, "../");
+
 module.exports = {
     entry: [
         path.join(__dirname, '../index.js')
-    ]
+    ],
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
 }
